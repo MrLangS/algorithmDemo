b56894f3bf811e44737bce46c40d49b42ef4483a
MrLang-xf <1723135507@qq.com>
2019-05-24 18:30:08 +0800

    5.24

--- src/leetcodealgorithm/algorithmicthinking/GreedyThought.java
+++ src/leetcodealgorithm/algorithmicthinking/GreedyThought.java
@@ -1,7 +1,9 @@
 package leetcodealgorithm.algorithmicthinking;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * GreedyThought
@@ -91,7 +93,7 @@ public class GreedyThought {
         Arrays.sort(points, Comparator.comparingInt(o -> o[1]));
         int cnt = 1, end = points[0][1];
         for (int i = 1; i < points.length; i++) {
-            if(points[i][0] <= end) {
+            if (points[i][0] <= end) {
                 continue;
             }
             cnt++;
@@ -101,12 +103,105 @@ public class GreedyThought {
     }
 
     /**
+     * 406. Queue Reconstruction by Height(Medium)
      * 一个学生用两个分量 (h, k) 描述，h 表示身高，k 表示排在前面的有 k 个学生的身高比他高或者和他一样高。
-     * 
+     *
      * @param people
      * @return
      */
     public int[][] reconstructQueue(int[][] people) {
+        if (people == null || people.length == 0 || people[0].length == 0) {
+            return new int[0][0];
+        }
+        Arrays.sort(people, (a, b) -> (a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]));
+        List<int[]> queue = new ArrayList<>();
+        for (int[] p : people) {
+            queue.add(p[1], p);
+        }
+        return queue.toArray(new int[queue.size()][]);
+    }
 
+    /**
+     * 121. Best Time to Buy and Sell Stock (Easy)
+     * 一次股票交易包含买入和卖出，只进行一次交易，求最大收益。
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int max = 0;
+        int curMin = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (curMin > prices[i]) {
+                curMin = prices[i];
+            } else {
+                max = Math.max(max, prices[i] - curMin);
+            }
+        }
+        return max;
+    }
+
+    /**
+     * 122. Best Time to Buy and Sell Stock II (Easy)
+     * 可以进行多次交易，多次交易之间不能交叉进行，可以进行多次交易。
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit02(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += (prices[i] - prices[i - 1]);
+            }
+        }
+        return profit;
+    }
+
+    /**
+     * 605. Can Place Flowers (Easy)
+     * flowerbed 数组中 1 表示已经种下了花朵。花朵之间至少需要一个单位的间隔，求解是否能种下 n 朵花。
+     *
+     * @param flowerbed
+     * @param n
+     * @return
+     */
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int len = flowerbed.length;
+        int cnt = 0;
+        for (int i = 0; i < len && cnt < n; i++) {
+            if (flowerbed[i] == 1) {
+                continue;
+            }
+            int pre = i == 0 ? 0 : flowerbed[i - 1];
+            int next = flowerbed[i + 1];
+            if (pre == 0 && next == 0) {
+                cnt++;
+                flowerbed[i] = 1;
+            }
+        }
+        return cnt >= n;
+    }
+
+    /**
+     * 392. Is Subsequence (Medium)
+     * 判断是否为子序列
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubsequence(String s, String t) {
+        int index = -1;
+        for (char c : s.toCharArray()) {
+            index = t.indexOf(c, index + 1);
+            if (index == -1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
